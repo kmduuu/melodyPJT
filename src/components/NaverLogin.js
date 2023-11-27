3f432cdc0a92c689a5a037c9168a36b263ed5fac
kokoku <happykimdw@naver.com>
2023-11-27 22:16:37 +0900

    second commit

--- src/components/NaverLogin.js
+++ src/components/NaverLogin.js
@@ -3,14 +3,25 @@
 import React from 'react';
 import NaverLogin from 'react-naver-login';
 import '../styles/Login.css';
+import { io } from 'socket.io-client';
 
 const NaverLoginButton = () => {
   const NaverClientId = 'vwTn9B_7NMCCeNpv4BxU';
+
+  const socket = io('http://192.168.55.20:3001');
 
   // 1. 로그인 성공 후의 동작
   const responseNaver = (response) => {
     var result = response;
     console.log(result);
+    alert("로그인 완료, 이제 기존 가입 회원인지 확인한다.");
+    socket.emit('naverLogin', result);
+    socket.on('message', (data) => {
+      // data 값은 0과 1로 구성하기
+      console.log('Received message from server : ', data);
+      //if문으로 확인 후... 맞으면
+      
+    })
   };
 
   const error = (error) => {
