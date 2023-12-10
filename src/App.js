c1752ab305cf928a639ec0357b652fce178280c4
kokoku <happykimdw@naver.com>
2023-12-10 11:24:13 +0900

    naverLogin Implemention

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 // App.js
 
 import React from 'react';
+import './App.css'; // CSS 파일 임포트
 import NaverLoginButton from './components/NaverLogin';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NaverLoginButton />
+        MELODY GAME
       </header>
+      <div> {/* 'body' 대신 'div' 사용 */}
+        <NaverLoginButton />
+      </div>
     </div>
   );
 }
--- src/components/NaverLogin.js
+++ src/components/NaverLogin.js
@@ -2,25 +2,24 @@
 
 import React from 'react';
 import NaverLogin from 'react-naver-login';
-import '../styles/Login.css';
+import '../styles/NaverLogin.css';
 import { io } from 'socket.io-client';
 
 const NaverLoginButton = () => {
   const NaverClientId = 'vwTn9B_7NMCCeNpv4BxU';
 
-  const socket = io('http://192.168.55.20:3001');
+  const socket = io('http://192.168.55.130:3001');
 
   // 1. 로그인 성공 후의 동작
   const responseNaver = (response) => {
     var result = response;
+    // alert("로그인 완료, 이제 기존 가입 회원인지 확인한다.");
     console.log(result);
-    alert("로그인 완료, 이제 기존 가입 회원인지 확인한다.");
     socket.emit('naverLogin', result);
+    console.log("다시 리액트로 돌아옴.");
+
     socket.on('message', (data) => {
-      // data 값은 0과 1로 구성하기
-      console.log('Received message from server : ', data);
-      //if문으로 확인 후... 맞으면
-      
+      console.log("불러온 값 : ", data);
     })
   };
 
@@ -28,12 +27,11 @@ const NaverLoginButton = () => {
     console.log(error);
   }
 
-
   return (
     <div className='centered'>
       <NaverLogin
         clientId={NaverClientId}
-        callbackUrl="http://localhost:3000/"
+        callbackUrl="http://localhost:3000/LoginSuccess"
         render={(props) => (
           <button className='naver-login-button' onClick={props.onClick}>
             네이버로 로그인
